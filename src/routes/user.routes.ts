7f9015641adf76e9749e1fb791e84fda3e986d7a
Dhrumil1434 <dhrumilpanchal1434@gmail.com>
2025-09-18 18:26:22 +0530

    feat: implemented route for the login and used the validateBody and controller

--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -1,11 +1,12 @@
 import { Router } from 'express';
 import { validateBody } from 'middlewares/zodSchema.validator.middleware';
 import { UserController } from 'modules/user/user.controller';
-import { registerSchema } from 'modules/user/user.zod';
+import { loginSchema, registerSchema } from 'modules/user/user.zod';
 const router = Router();
 router.post(
   '/register',
   validateBody(registerSchema),
   UserController.registerUser
 );
+router.post('/login', validateBody(loginSchema), UserController.loginUser);
 export default router;
