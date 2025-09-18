152a2f42852f9effedf75727afce06c7d4ac07ec
Dhrumil1434 <dhrumilpanchal1434@gmail.com>
2025-09-18 22:16:54 +0530

    fix: formatted using format command

--- tests/auth.register.api.test.ts
+++ tests/auth.register.api.test.ts
@@ -19,14 +19,12 @@ describe('POST /api/auth/user/register', () => {
       deletedAt: null,
     } as any);
 
-    const res = await request(app)
-      .post('/api/auth/user/register')
-      .send({
-        name: 'Max',
-        email: 'max@example.com',
-        password: 'Abcd1234@',
-        role: 'customer',
-      });
+    const res = await request(app).post('/api/auth/user/register').send({
+      name: 'Max',
+      email: 'max@example.com',
+      password: 'Abcd1234@',
+      role: 'customer',
+    });
 
     expect(res.status).toBe(201);
     expect(res.body.success).toBe(true);
@@ -42,14 +40,12 @@ describe('POST /api/auth/user/register', () => {
   it('409 when email already exists', async () => {
     vi.spyOn(userRepo.userRepository, 'isEmailTaken').mockResolvedValue(true);
 
-    const res = await request(app)
-      .post('/api/auth/user/register')
-      .send({
-        name: 'Max',
-        email: 'dup@example.com',
-        password: 'Abcd1234@',
-        role: 'customer',
-      });
+    const res = await request(app).post('/api/auth/user/register').send({
+      name: 'Max',
+      email: 'dup@example.com',
+      password: 'Abcd1234@',
+      role: 'customer',
+    });
 
     expect(res.status).toBe(409);
     expect(res.body.success).toBe(false);
