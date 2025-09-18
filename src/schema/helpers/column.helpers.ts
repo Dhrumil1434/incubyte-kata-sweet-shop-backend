c1045063f09f4627ace9382b40cc723abe23b021
Dhrumil1434 <dhrumilpanchal1434@gmail.com>
2025-09-18 16:46:24 +0530

    fix: is_active to isActive for the better consistency

--- src/schema/helpers/column.helpers.ts
+++ src/schema/helpers/column.helpers.ts
@@ -3,7 +3,7 @@ import { boolean, timestamp, datetime } from 'drizzle-orm/mysql-core';
 
 // Common column helpers to be reused across tables
 export const timestamps = {
-  is_active: boolean('is_active').default(true),
+  isActive: boolean('is_active').default(true),
   createdAt: timestamp('created_at', { mode: 'string' })
     .notNull()
     .default(sql`CURRENT_TIMESTAMP`),
