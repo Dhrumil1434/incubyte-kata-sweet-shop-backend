44497515c07e7f6ed8500fcbd13cf3ad7260b445
Dhrumil1434 <dhrumilpanchal1434@gmail.com>
2025-09-18 17:15:57 +0530

    fix: modified the api name to the register

--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -3,5 +3,9 @@ import { validateBody } from 'middlewares/zodSchema.validator.middleware';
 import { UserController } from 'modules/user/user.controller';
 import { registerSchema } from 'modules/user/user.zod';
 const router = Router();
-router.post('/', validateBody(registerSchema), UserController.registerUser);
+router.post(
+  '/register',
+  validateBody(registerSchema),
+  UserController.registerUser
+);
 export default router;
