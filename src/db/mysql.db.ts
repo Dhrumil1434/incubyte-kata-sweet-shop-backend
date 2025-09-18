8aa924bcce634ccd33efceb80d3416a96a74439b
Dhrumil1434 <dhrumilpanchal1434@gmail.com>
2025-09-18 13:01:46 +0530

    feat: added mysql db connector file

---
+++ src/db/mysql.db.ts
@@ -0,0 +1,11 @@
+import { drizzle } from 'drizzle-orm/mysql2';
+import mysql from 'mysql2/promise';
+import dotenv from 'dotenv';
+dotenv.config();
+
+const poolConnection = mysql.createPool({
+  uri: process.env['DATABASE_URL']!,
+});
+
+const db = drizzle(poolConnection, { casing: 'snake_case' });
+export { db, poolConnection };
