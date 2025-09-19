dc46668ec12aa687293bc52d1153ca3f09676a81
Dhrumil1434 <dhrumilpanchal1434@gmail.com>
2025-09-20 04:26:13 +0530

    feat: removed the restock db

--- src/drizzle/migrations/relations.ts
+++ src/drizzle/migrations/relations.ts
@@ -1,5 +1,5 @@
 import { relations } from 'drizzle-orm/relations';
-import { sweets, purchases, users, restocks, categories } from './schema';
+import { sweets, purchases, users, categories } from '../../schema';
 
 export const purchasesRelations = relations(purchases, ({ one }) => ({
   sweet: one(sweets, {
@@ -14,7 +14,6 @@ export const purchasesRelations = relations(purchases, ({ one }) => ({
 
 export const sweetsRelations = relations(sweets, ({ one, many }) => ({
   purchases: many(purchases),
-  restocks: many(restocks),
   category: one(categories, {
     fields: [sweets.categoryId],
     references: [categories.id],
@@ -23,18 +22,6 @@ export const sweetsRelations = relations(sweets, ({ one, many }) => ({
 
 export const usersRelations = relations(users, ({ many }) => ({
   purchases: many(purchases),
-  restocks: many(restocks),
-}));
-
-export const restocksRelations = relations(restocks, ({ one }) => ({
-  user: one(users, {
-    fields: [restocks.adminId],
-    references: [users.id],
-  }),
-  sweet: one(sweets, {
-    fields: [restocks.sweetId],
-    references: [sweets.id],
-  }),
 }));
 
 export const categoriesRelations = relations(categories, ({ many }) => ({
